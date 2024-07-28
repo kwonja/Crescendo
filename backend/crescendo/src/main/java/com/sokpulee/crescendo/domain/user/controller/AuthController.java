2e4e9a6fbf19c08628de873feefdb225e3762885
Sanizzang <tksl4732@gmail.com>
2024-07-28 22:35:33 +0900

    feat: 회원가입 API Swagger 설정 추가

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/user/controller/AuthController.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/user/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.sokpulee.crescendo.domain.user.dto.request.EmailRandomKeyRequest;
 import com.sokpulee.crescendo.domain.user.dto.request.SignUpRequest;
 import com.sokpulee.crescendo.domain.user.dto.response.EmailRandomKeyResponse;
 import com.sokpulee.crescendo.domain.user.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "사용자 인증 관련 API")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입", description = "회원가입 API")
     public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
         authService.signUp(signUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
