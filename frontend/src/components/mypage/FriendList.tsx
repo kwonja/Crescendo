8edb0ffd9bac03b9db85d79c90b776adbc421334
kwonja <kownja87@gmail.com>
2024-07-30 00:23:23 +0900

    [FEAT] 팔로우/팔로워 구현

--- frontend/src/components/mypage/FriendList.tsx
+++ frontend/src/components/mypage/FriendList.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { ReactComponent as Search } from '../../assets/images/search.svg';
+import FriendProfile from './FriendProfile';
 export default function FriendList() {
+  const [isSelected,setIsSelected] =useState<string>('left');
   return (
-    <div className='friendlist'>
-        <div className='listbar'>
-        <div className='follow left'>팔로워</div>
-        <div className='follow right'>팔로워</div>
+    <>
+    <div className='friend'>
+          <div className='listbar'>
+            <div className={`follow left ${isSelected === 'left' ? 'active' :''}`} 
+              onClick={ ()=>setIsSelected('left')}
+            >
+            <div>팔로우</div>
+            <span>1000</span>
+            </div>
+          <div className={`follow right ${isSelected === 'right' ? 'active' :''}`} 
+          onClick={ ()=>setIsSelected('right')}
+          >
+          <div>팔로워</div>
+          <span>500</span>
+          </div>
         </div>
     </div>
+    <div className='list'>
+
+      <div className="search-container">
+        <span><input type="text" placeholder="친구를 검색하세요"/>
+        <div className='search-icon'><Search/></div>
+        </span>
+      </div>
+
+      <div className='profilelist'>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+        <FriendProfile/>
+      </div>
+    </div>
+    </>
+    
   )
 }
