f3612818728b6d8d9a4e83871f5095cb32e738ea
kwonja <kownja87@gmail.com>
2024-07-29 17:57:39 +0900

    [FEAT] 마이페이지 친구 리스트

---
+++ frontend/src/components/mypage/FriendList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FriendList() {
+  return (
+    <div className='friendlist'>
+        <div className='listbar'>
+        <div className='follow left'>팔로워</div>
+        <div className='follow right'>팔로워</div>
+        </div>
+    </div>
+  )
+}
--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Profile from '../components/mypage/Profile'
+import FriendList from '../components/mypage/FriendList'
 
 export default function MyPage() {
   return (
     <div className='mypage'>
     <div className='mypage_left'>
         <Profile/>
+        <FriendList/>
     </div>
     <div className='mypage_center'></div>
     </div>
