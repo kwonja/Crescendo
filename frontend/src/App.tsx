0cde61700acc2cc33b081659d9eef7a7f18b877b
kwonja <kownja87@gmail.com>
2024-07-30 14:24:48 +0900

    [STYLE] : 로그인헤더

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './components/header/NotLoginHeader';
-// import Header from './components/header/LoginHeader';
+// import Header from './components/header/NotLoginHeader';
+import Header from './components/header/LoginHeader';
 
 export default function App() {
   return (
