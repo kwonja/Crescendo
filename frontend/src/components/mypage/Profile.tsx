7b1adce505c0926dfc2c1d80d3b02a7b9637df8f
kwonja <kownja87@gmail.com>
2024-07-30 10:20:54 +0900

    [FEAT] 마이페이지 피드/갤러리 탭 구현

--- frontend/src/components/mypage/Profile.tsx
+++ frontend/src/components/mypage/Profile.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { ReactComponent as Edit } from '../../assets/images/edit.svg';
+// import { ReactComponent as Edit } from '../../assets/images/edit.svg';
 export default function Profile() {
   return (
     <div className='profile'>
         <div className='img'>
             <img src="https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg" alt="유저 프로필"/>
-            <div className='edit'><Edit className='icon'/></div>
+            {/* <div className='edit'><Edit className='icon'/></div> */}
         </div>
         <div className='nickname'>
              Nickname
--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,15 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Profile from '../components/mypage/Profile'
 import FriendList from '../components/mypage/FriendList'
-
+import { ReactComponent as Crown } from '../assets/images/crown.svg';
 export default function MyPage() {
+  const [isSelected,setIsSelected] = useState<string>('feed');
+  const [indicatorStyle, setIndicatorStyle] = useState<React.CSSProperties>({});
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("테스트")
+    const menuElement = menuRef.current;
+    if (menuElement) {
+      const activeLink = menuElement.querySelector('.active') as HTMLElement;
+      if (activeLink) {
+        const { offsetLeft, offsetWidth} = activeLink;
+        setIndicatorStyle({
+          left: offsetLeft + (offsetWidth - 160)/2 + 'px', // Center the indicator
+        });
+      }
+    }
+  }, [isSelected]);
+
   return (
     <div className='mypage'>
     <div className='mypage_left'>
         <Profile/>
         <FriendList/>
     </div>
-    <div className='mypage_center'></div>
+    <div className='mypage_center'>
+
+      <div className='myfavorite'>
+         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7Gh48vV6C_LMRFTzwuoR8lD4y_K20Uf09nQ&s" alt="최애"/>
+         <div className='crown'><Crown/></div>
+         <div className='text'>NewJeans</div>
+      </div>
+
+
+
+      <div className='category' ref={menuRef}>
+        <div className={`item ${isSelected === 'feed' ? 'active' : ''}`} onClick={()=>setIsSelected('feed')}>내 피드</div>   
+        <div className={`item ${isSelected === 'gallery' ? 'active' : ''}`} onClick={()=>setIsSelected('gallery')}>내 갤러리</div>
+        <div className="indicator" style={indicatorStyle}></div>
+      </div>
+      
+
+    </div>
     </div>
   )
 }
