61620e6329caad13f9f6ff26aed5c094ea800d8b
kwonja <kownja87@gmail.com>
2024-07-31 16:24:14 +0900

    feat : 마이페이지 피드보기

--- frontend/src/components/common/Feed.tsx
+++ frontend/src/components/common/Feed.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { ReactComponent as User } from '../../assets/images/reduser.svg';
-import { ReactComponent as Heart } from '../../assets/images/heart.svg';
-import { ReactComponent as Dots } from '../../assets/images/dots.svg';
-import { ReactComponent as Comment } from '../../assets/images/comment.svg';
+import React, { useState } from 'react';
+import { ReactComponent as User } from '../../assets/images/Feed/reduser.svg';
+import { ReactComponent as Heart } from '../../assets/images/Feed/heart.svg';
+import { ReactComponent as Dots } from '../../assets/images/Feed/dots.svg';
+import { ReactComponent as Comment } from '../../assets/images/Feed/comment.svg';
+import { ReactComponent as FullHeart } from '../../assets/images/Feed/fullheart.svg';
 export default function Feed() {
+  const [like,setLike]= useState<number>(99);
+  const [comment,setComment]= useState<number>(99);
+  const [isLiked,setisLiked]=useState<boolean>(false);
   const tags = ['2주년', '뉴진스'];
 
   return (
@@ -14,7 +18,7 @@ export default function Feed() {
           <span>Nickname</span>
           <span>2024.01.01</span>
         </div>
-        <Heart className="heart" />
+       
         <Dots className="dots" />
       </div>
       <div className="text">뉴진스와 버니즈의 2주년!!</div>
@@ -23,7 +27,17 @@ export default function Feed() {
           <div key={index}>#{tag}</div>
         ))}
       </div>
-      <Comment className="comment" />
+      <div className="heart">{like} 
+        {
+          !isLiked ? <Heart onClick={()=>{ setLike(prev=>prev+1); setisLiked(true)}}/> : 
+          <FullHeart
+          onClick={()=>{ setLike(prev=>prev-1);setisLiked(false)}}
+          />
+        }
+        </div>
+      <div className="comment"> {comment} 
+        <Comment onClick={()=>{setComment(prev=>prev+1)}}/>  
+        </div>
     </div>
   );
 }
