12b118560a01faf43d0f42e9018f9b4976a6dabe
하상진 <lavegas1@naver.com>
2024-08-01 13:38:12 +0900

    feat: 팬아트 글수정 api 구현

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/fanart/controller/FanArtController.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/fanart/controller/FanArtController.java
@@ -2,11 +2,13 @@ package com.sokpulee.crescendo.domain.fanart.controller;
 
 import com.sokpulee.crescendo.domain.fanart.dto.request.FanArtAddRequest;
 import com.sokpulee.crescendo.domain.fanart.dto.request.FanArtCommentAddRequest;
+import com.sokpulee.crescendo.domain.fanart.dto.request.FanArtUpdateRequest;
 import com.sokpulee.crescendo.domain.fanart.service.FanArtService;
 import com.sokpulee.crescendo.domain.feed.dto.request.FeedAddRequest;
 import com.sokpulee.crescendo.global.auth.annotation.AuthPrincipal;
 import com.sokpulee.crescendo.global.exception.custom.AuthenticationRequiredException;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +32,7 @@ public class FanArtController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "팬아트 글쓰기", description = "팬아트 글쓰기 API")
     public ResponseEntity<?> addFanArt(
-            @AuthPrincipal Long loggedInUserId,
+            @Parameter(hidden = true) @AuthPrincipal Long loggedInUserId,
             @RequestParam("title") String title,
             @RequestParam("content") String content,
             @RequestParam(value = "imageList",required = false) List<MultipartFile> imageList,
@@ -49,7 +51,7 @@ public class FanArtController {
     @DeleteMapping("/{fan-art-id}")
     @Operation(summary = "팬아트 글삭제", description = "팬아트 글삭제 API")
     public ResponseEntity<?> deleteFanArt(
-            @AuthPrincipal Long loggedInUserId,
+            @Parameter(hidden = true) @AuthPrincipal Long loggedInUserId,
             @PathVariable("fan-art-id") Long fanArtId
     ){
         if(loggedInUserId == null) {
@@ -58,12 +60,29 @@ public class FanArtController {
         fanArtService.deleteFanArt(fanArtId);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "{fan-art-id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "팬아트 글수정", description = "팬아트 글수정 API")
+    public ResponseEntity<?> updateFanArt(
+            @Parameter(hidden = true) @AuthPrincipal Long loggedInUserId,
+            @PathVariable("fan-art-id") Long fanArtId,
+            @Valid @ModelAttribute FanArtUpdateRequest updateRequest
+            ){
+        if(loggedInUserId == null) {
+            throw new AuthenticationRequiredException();
+        }
+
+        fanArtService.updateFanArt(loggedInUserId,fanArtId,updateRequest);
+
+        return ResponseEntity.ok().build();
+
+    }
     
 
     @PostMapping("/{fan-art-id}/comment")
     @Operation(summary = "팬아트 댓글쓰기", description = "팬아트 댓글쓰기 API")
     public ResponseEntity<?> addFanArtComment(
-            @AuthPrincipal Long loggedInUserId,
+            @Parameter(hidden = true) @AuthPrincipal Long loggedInUserId,
             @PathVariable("fan-art-id") Long fanArtId,
             @RequestParam String content
     ){
@@ -81,7 +100,7 @@ public class FanArtController {
     @PostMapping("{fan-art-id}/comment/{fan-art-comment-id}/reply")
     @Operation(summary = "팬아트 답글쓰기", description = "팬아트 답글쓰기 API")
     public ResponseEntity<?> addFanArtReply(
-            @AuthPrincipal Long loggedInUserId,
+            @Parameter(hidden = true) @AuthPrincipal Long loggedInUserId,
             @PathVariable("fan-art-id") Long fanArtId,
             @PathVariable("fan-art-comment-id") Long fanArtCommentId,
             @RequestParam String content
