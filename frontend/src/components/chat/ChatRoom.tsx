1264364663c2986e08c8fc3c4e837f4c4430396a
kwonja <kownja87@gmail.com>
2024-08-02 00:47:19 +0900

    feat : 채팅방, 채팅메세지 UI 구현

--- frontend/src/components/chat/ChatRoom.tsx
+++ frontend/src/components/chat/ChatRoom.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { ReactComponent as Back } from '../../assets/images/Chat/back.svg';
 import { ReactComponent as Hamburger } from '../../assets/images/Chat/hamburger.svg';
+import { ReactComponent as Line } from '../../assets/images/Chat/line.svg';
+import { ReactComponent as Clip } from '../../assets/images/Chat/clip.svg';
+import { ReactComponent as Submit } from '../../assets/images/Chat/submit.svg';
+import MyMessage from './MyMessage';
+import OtherMessage from './OtherMessage';
 export default function Chatroom() {
   return (
     <div className='chatroom'>
@@ -9,7 +14,27 @@ export default function Chatroom() {
         <div>권자몬</div>
         <Hamburger/>
         </div>
+        <div className='date'>
+          <Line style={{width : 20}}/>
+          <div>2024 년 07 월 24 일</div>
+          <Line/>
+        </div>
+        <OtherMessage/>
+        <MyMessage/>
         
+        <div className="send-container">
+        <span>
+        <input
+          type="text"
+          className="search-input"
+          placeholder='여기에 입력하세요'
+        />
+        <div className="send-icon">
+          <Clip style={{width : "24px", height : "24px"}}/>
+          <Submit style={{width : "24px", height : "24px"}}/>
+        </div>
+      </span>
+    </div>
     </div>
   )
 }
---
+++ frontend/src/components/chat/OtherMessage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OtherMessage() {
+  return (
+    <div className='OtherMessage'>
+    <span>안녕하세요!</span>
+    </div>
+  )
+}
