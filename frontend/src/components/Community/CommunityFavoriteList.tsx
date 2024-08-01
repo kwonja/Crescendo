1ae47cadc57ee5ab1b009d0930a7e20fa40684e3
kwonja <kownja87@gmail.com>
2024-08-01 10:43:21 +0900

    merge : 마이페이지 피드 머지

--- frontend/src/components/Community/CommunityFavoriteList.tsx
+++ frontend/src/components/Community/CommunityFavoriteList.tsx
@@ -13,81 +13,102 @@ export default function CommunityFavoriteList() {
   };
 
   // 임시 데이터
-  const tmpList:communityInfo[] = [ // 현재 페이지의 데이터 목록 (커뮤니티 목록)
+  const tmpList: communityInfo[] = [
+    // 현재 페이지의 데이터 목록 (커뮤니티 목록)
     {
       idolGroupId: 1,
-      name: "NewJeans",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: 'NewJeans',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
     {
       idolGroupId: 2,
-      name: "BTS",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: 'BTS',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
     {
       idolGroupId: 3,
-      name: "Aespa",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: 'Aespa',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
     {
       idolGroupId: 4,
-      name: "Black Pink",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: 'Black Pink',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
     {
       idolGroupId: 5,
-      name: "소녀시대",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: '소녀시대',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
     {
       idolGroupId: 6,
-      name: "오마이걸",
-      profile: "https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg"
+      name: '오마이걸',
+      profile: 'https://i.ibb.co/t3rdL7G/313885-438531-4716.jpg',
     },
   ];
-  
+
   // 상수 또는 변수(상태) 초기화
   const SIZE_PER_PAGE = 4; // 한번에 몇개의 그룹이 보일지
   const MOVE_STEP = 4; // 화살표 클릭시 몇개의 그룹을 넘길지
 
   const [idx, setIdx] = useState<number>(-1); // 로딩 전 -1
-  const [communityList, setCommunityList] = useState<communityInfo[]>([])
-  const [showList, setShowList] = useState<communityInfo[]>([])
+  const [communityList, setCommunityList] = useState<communityInfo[]>([]);
+  const [showList, setShowList] = useState<communityInfo[]>([]);
 
   // 리스트 불러오기
-  useEffect(() =>{
+  useEffect(() => {
     // 테스트용 코드
     setCommunityList(tmpList);
-    setIdx(0);  
-  },[]);
+    setIdx(0);
+  }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (idx === -1) return; // 로딩 전 로딩안함
     let tmp = [...communityList];
-    tmp = tmp.slice(idx, idx+SIZE_PER_PAGE);
+    tmp = tmp.slice(idx, idx + SIZE_PER_PAGE);
     setShowList(tmp);
   }, [idx]);
 
-  
   function increaseIdx() {
-    if (idx+SIZE_PER_PAGE+MOVE_STEP < communityList.length-1) setIdx((prev)=>prev+MOVE_STEP);
+    if (idx + SIZE_PER_PAGE + MOVE_STEP < communityList.length - 1)
+      setIdx(prev => prev + MOVE_STEP);
     else {
-      setIdx(communityList.length-SIZE_PER_PAGE);
+      setIdx(communityList.length - SIZE_PER_PAGE);
     }
   }
 
   function decreaseIdx() {
-    if (idx-MOVE_STEP > 0) setIdx((prev)=>prev-MOVE_STEP);
+    if (idx - MOVE_STEP > 0) setIdx(prev => prev - MOVE_STEP);
     else {
       setIdx(0);
     }
   }
 
-  return <div className='communityfavoritelist_container'>
-    {<Button className={`square empty ${idx===0?'hidden ':''}`} onClick={decreaseIdx}><LeftBtn/></Button>}
-    <div className='communityfavoritelist_contents'>
-      {showList.map((community)=>(<CommunityCard idolGroupId={community.idolGroupId} name={community.name} profile={community.profile} key={community.idolGroupId} />))}
+  return (
+    <div className="communityfavoritelist_container">
+      {
+        <Button className={`square empty ${idx === 0 ? 'hidden ' : ''}`} onClick={decreaseIdx}>
+          <LeftBtn />
+        </Button>
+      }
+      <div className="communityfavoritelist_contents">
+        {showList.map(community => (
+          <CommunityCard
+            idolGroupId={community.idolGroupId}
+            name={community.name}
+            profile={community.profile}
+            key={community.idolGroupId}
+          />
+        ))}
+      </div>
+      {
+        <Button
+          className={`square empty ${idx >= communityList.length - 4 ? 'hidden ' : ''}`}
+          onClick={increaseIdx}
+        >
+          <RightBtn />
+        </Button>
+      }
     </div>
-    {<Button className={`square empty ${idx>=communityList.length-4?'hidden ':''}`} onClick={increaseIdx}><RightBtn/></Button>}
-  </div>;
+  );
 }
--- frontend/src/pages/CommunityMain.tsx
+++ frontend/src/pages/CommunityMain.tsx
@@ -1,24 +1,23 @@
-import CommunityFavoriteList from "../components/Community/CommunityFavoriteList";
-import CommunityList from "../components/Community/CommunityList";
-import CommunitySearchInput from "../components/Community/CommunitySearchInput";
-
+import CommunityFavoriteList from '../components/Community/CommunityFavoriteList';
+import CommunityList from '../components/Community/CommunityList';
+import CommunitySearchInput from '../components/Community/CommunitySearchInput';
 
 export default function CommunityMain() {
-
-  return <div className='communitymain'>
-    <div className='communitymain_contents'>
-      <div className='communitymain_title'>MY 커뮤니티</div>
-    </div>
-    <CommunityFavoriteList/>
-    <div className='communitymain_contents'>
-      <div className='communitymain_title'>ALL 커뮤니티</div>
-      <div className='communitymain_searchbar'>
-        <CommunitySearchInput />
+  return (
+    <div className="communitymain">
+      <div className="communitymain_contents">
+        <div className="communitymain_title">MY 커뮤니티</div>
+      </div>
+      <CommunityFavoriteList />
+      <div className="communitymain_contents">
+        <div className="communitymain_title">ALL 커뮤니티</div>
+        <div className="communitymain_searchbar">
+          <CommunitySearchInput />
+        </div>
+      </div>
+      <div className="communitymain_contents">
+        <CommunityList />
       </div>
     </div>
-    <div className='communitymain_contents'>
-      <CommunityList/>
-    </div>
-    
-  </div>;
+  );
 }
