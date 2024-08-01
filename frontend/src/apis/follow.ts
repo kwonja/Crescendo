ae6a82b3dc0968d4df6c34aa8e42fac3d6b59db3
kwonja <kownja87@gmail.com>
2024-08-01 16:02:39 +0900

    feat : 팔로우 팔로잉 에러처리 구현

--- frontend/src/apis/follow.ts
+++ frontend/src/apis/follow.ts
@@ -1,11 +1,11 @@
-import { api } from "./core"
+import { api } from './core';
 
-export const followerAPI = async(userId : number)=>{
-    const response = await api.get(`/api/v1/follow/user/${userId}/follower`);
-    return response.data;
-}
+export const followerAPI = async (userId: number) => {
+  const response = await api.get(`/api/v1/follow/user/${userId}/follower`);
+  return response.data;
+};
 
-export const followingAPI = async(userId : number)=>{
-    const response = await api.get(`/api/v1/follow/user/${userId}/following`);
-    return response.data;
-}
+export const followingAPI = async (userId: number) => {
+  const response = await api.get(`/api/v1/follow/user/${userId}/following`);
+  return response.data;
+};
--- frontend/src/features/follow/followingSlice.ts
+++ frontend/src/features/follow/followingSlice.ts
@@ -3,71 +3,66 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import { follow } from '../../interface/follow';
-import { followingAPI} from '../../apis/follow';
-
+import { followingAPI } from '../../apis/follow';
 
 type PromiseStatus = 'loading' | 'sucess' | 'failed' | '';
-interface followProps{
-    followingList : follow[],
-    status : PromiseStatus,
-    error : string | undefined
+interface followProps {
+  followingList: follow[];
+  status: PromiseStatus;
+  error: string | undefined;
 }
 
 //createAsyncThunk는 비동기 작업을 도와주는 액션함수이기때문에
 //타입이 있어야한다.
 export const getUserFollowing = createAsyncThunk(
-    'followSlice/getUserFollowing',
-    async (userId : number) => {
-        const response = await followingAPI(userId);
-        return response;
-    }
+  'followSlice/getUserFollowing',
+  async (userId: number) => {
+    const response = await followingAPI(userId);
+    return response;
+  },
 );
 
-
-
-
-const inistalState : followProps ={
-    followingList :[
-        {
-            userId : 1,
-            nickname : 'Nickname1',
-            profilePath : 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
-        },
-        {
-            userId : 1,
-            nickname : 'Nickname1',
-            profilePath : 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
-        },
-        {
-            userId : 1,
-            nickname : 'Nickname1',
-            profilePath : 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
-        }
-    ],
-    status :'',
-    error : ''
-}
+const inistalState: followProps = {
+  followingList: [
+    {
+      userId: 1,
+      nickname: 'Nickname1',
+      profilePath: 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
+    },
+    {
+      userId: 1,
+      nickname: 'Nickname1',
+      profilePath: 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
+    },
+    {
+      userId: 1,
+      nickname: 'Nickname1',
+      profilePath: 'https://cdn.topstarnews.net/news/photo/202301/15040596_1067813_363.jpg',
+    },
+  ],
+  status: '',
+  error: '',
+};
 
 const followerSlice = createSlice({
   name: 'follwer',
   initialState: inistalState,
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: builder => {
     builder
-     .addCase(getUserFollowing.pending, (state) => {
+      .addCase(getUserFollowing.pending, state => {
         state.status = 'loading';
       })
-     .addCase(getUserFollowing.fulfilled, (state, action) => {
-        state.status ='sucess';
+      .addCase(getUserFollowing.fulfilled, (state, action) => {
+        state.status = 'sucess';
         console.log(action.payload);
         state.followingList = action.payload.followingList;
       })
-     .addCase(getUserFollowing.rejected, (state, action) => {
+      .addCase(getUserFollowing.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
   },
 });
-
 
 export default followerSlice.reducer;
