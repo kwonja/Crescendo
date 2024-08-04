664c8d5e99fe3fc230ec92182eb2483d4268e208
김진현 <kimjh9903@naver.com>
2024-08-05 00:41:11 +0900

    fix: observer 자원 반환 위치 변경 & feat: 페이지 리셋 추가

--- frontend/src/components/community/CommunityList.tsx
+++ frontend/src/components/community/CommunityList.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useRef} from 'react';
+import React, { useCallback, useEffect, useRef} from 'react';
 import CommunityCard from './CommunityCard';
 import { useAppSelector, useAppDispatch } from '../../store/hooks/hook';
-import { getCommunityList } from '../../features/communityList/communityListSlice';
+import { getCommunityList, resetPage } from '../../features/communityList/communityListSlice';
 
 export default function CommunityList() {
   const {communityList, page, hasMore, status} = useAppSelector(state => state.communityList);
@@ -9,6 +9,12 @@ export default function CommunityList() {
   const observer = useRef<IntersectionObserver | null>(null);
   const SIZE_PER_PAGE = 4;
 
+  useEffect(()=> {
+    return () => {
+      dispatch(resetPage());
+      if (observer.current) observer.current.disconnect();
+    }
+  }, [])
 
   const loadMoreElementRef = useCallback((node: HTMLDivElement | null) => {
     if (observer.current) observer.current.disconnect();
@@ -23,10 +29,7 @@ export default function CommunityList() {
       observer.current.observe(node);
     }
 
-    return () => {
-      if (observer.current) observer.current.disconnect();
-    };
-  }, [hasMore, page, dispatch]);
+  }, [hasMore, page, dispatch, status]);
 
 
   return (
