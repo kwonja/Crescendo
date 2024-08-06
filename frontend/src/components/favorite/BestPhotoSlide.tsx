ae870f38eda69d53745748ac75888ecc817b18a2
김진현 <kimjh9903@naver.com>
2024-08-06 11:09:20 +0900

    feat: 최애자랑/베스트슬라이드 구현

--- frontend/src/components/favorite/BestPhotoSlide.tsx
+++ frontend/src/components/favorite/BestPhotoSlide.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 
 
 type bestPhotoInfo = {
@@ -10,33 +11,58 @@ type bestPhotoInfo = {
 export default function BestPhotoSlide () {
 
     //임시 데이터
-    const bestPhotoList:bestPhotoInfo[] = [
+    const tmpList:bestPhotoInfo[] = [
         {
             idolId: 1,
             idolGroupName: "NewJeans",
             idolName: "민지",
-            favoriteIdolImagePath: ''
+            favoriteIdolImagePath: 'https://i.ibb.co/JFqgZck/minji.jpg'
         },
         {
             idolId: 2,
             idolGroupName: "NewJeans",
             idolName: "해린",
-            favoriteIdolImagePath: ''
+            favoriteIdolImagePath: 'https://i.ibb.co/qnNRwbY/haerin.jpg'
         },
         {
             idolId: 3,
             idolGroupName: "NewJeans",
             idolName: "하니",
-            favoriteIdolImagePath: ''
-        }
+            favoriteIdolImagePath: 'https://i.ibb.co/FbVfHxH/hani.jpg'
+        },
     ]
+    
+    const [bestPhotoList, setBestPhotoList] = useState<bestPhotoInfo[]>([]);
+    const [animationDuration, setAnimationDuration] = useState<number>(10);
+    const ANIMATION_SPEED = 150;
+
+    useEffect(()=>{
+        const getBestPhotoList = async () => {
+            const tmp = [...tmpList, ...tmpList];
+            setBestPhotoList(tmp);
+        }
+        getBestPhotoList();
+    }, [])
+
+    useEffect(()=>{
+        if (bestPhotoList.length === 0) return;
+        let totalWidth = bestPhotoList.length * 450;
+        setAnimationDuration(totalWidth/ANIMATION_SPEED);
+    }, [bestPhotoList])
 
     return (
-        <div>
-            {bestPhotoList.map((bestPhoto,idx)=>(<div key={idx}>
-                <img src={bestPhoto.favoriteIdolImagePath} alt={bestPhoto.idolName} />
-                <div>{bestPhoto.idolGroupName}-{bestPhoto.idolName}</div>
-            </div>))}
+        <div className="bestphotoslide_container">
+          <div className='bestphotoslide_title'>Best Photos</div>
+          <div className="bestphotoslide_track_container">
+            <div className="bestphotoslide_track" style={{ animation: `slide ${animationDuration}s linear infinite` }}>
+                {bestPhotoList.map((bestPhoto,idx)=>(
+                <div className='bestphotoslide_card' key={idx}>
+                    <img src={bestPhoto.favoriteIdolImagePath} alt={bestPhoto.idolName} />
+                    <div className='bestphotoslide_idolname'>{bestPhoto.idolGroupName}-{bestPhoto.idolName}</div>
+                </div>
+                ))}
+            </div>
+          </div>
         </div>
     )
 }
