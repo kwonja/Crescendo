4313e5baf76600a9294c6ad716f736f28d0283cc
김인제 <kij723@naver,com>
2024-08-07 12:29:39 +0900

    refacotr: 클래스 초기화

--- Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
@@ -1,13 +1,9 @@
 package com.sokpulee.idolcrawling.crawling.service;
 
-import com.sokpulee.idolcrawling.crawling.repository.CrawlingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CrawlingServiceImpl implements CrawlingService {
-
-    private final CrawlingRepository crawlingRepository;
-
 }
