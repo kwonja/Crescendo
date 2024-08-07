0fa431807b384b01d7fad26ff22ed10bd3f612e4
김인제 <kij723@naver,com>
2024-08-07 12:32:33 +0900

    feat: table 가져오기 구현

--- Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingService.java
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingService.java
@@ -1,4 +1,11 @@
 package com.sokpulee.idolcrawling.crawling.service;
 
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public interface CrawlingService {
+
+    Elements getTables(String param) throws IOException;
+
 }
--- Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
@@ -1,9 +1,26 @@
 package com.sokpulee.idolcrawling.crawling.service;
 
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class CrawlingServiceImpl implements CrawlingService {
+
+    @Value("${BASE_CRAWLING_URL}")
+    private String baseCrawlingUrl;
+
+    @Override
+    public Elements getTables(String param) throws IOException {
+        String a = baseCrawlingUrl + param;
+        Document html = Jsoup.connect(baseCrawlingUrl + param).get();
+        return html.select("body > div > div > div > main > div > div > div > table");
+    }
+
 }
