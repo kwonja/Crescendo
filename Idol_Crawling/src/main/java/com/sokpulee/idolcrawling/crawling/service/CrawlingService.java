f019981a53c86e2a75e5172af4755bbefa67f6cf
김인제 <kij723@naver,com>
2024-08-07 12:34:30 +0900

    feat: 아이돌 그룹 파라미터 리스트 가져오기 구현

--- Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingService.java
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingService.java
@@ -3,9 +3,12 @@ package com.sokpulee.idolcrawling.crawling.service;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface CrawlingService {
 
     Elements getTables(String param) throws IOException;
+
+    List<String> getIdolGroupParamList(Elements tables, String tag);
 
 }
--- Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/service/CrawlingServiceImpl.java
@@ -3,11 +3,14 @@ package com.sokpulee.idolcrawling.crawling.service;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,23 @@ public class CrawlingServiceImpl implements CrawlingService {
         String a = baseCrawlingUrl + param;
         Document html = Jsoup.connect(baseCrawlingUrl + param).get();
         return html.select("body > div > div > div > main > div > div > div > table");
+    }
+
+    @Override
+    public List<String> getIdolGroupParamList(Elements tables, String tag) {
+        List<String> idolGroupParamList = new ArrayList<>();
+
+        for (Element table : tables) {
+            Elements rows = table.select("tbody > tr");
+
+            for (int i = 1; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                idolGroupParamList.add(row.select(tag).attr("href"));
+            }
+
+        }
+
+        return idolGroupParamList;
     }
 
 }
