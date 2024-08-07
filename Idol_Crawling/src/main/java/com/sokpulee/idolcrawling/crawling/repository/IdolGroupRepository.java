e3f93fa9036b88bddeb35e96aa6be365a665d87d
김인제 <kij723@naver,com>
2024-08-07 15:10:30 +0900

    feat: 아이돌 그룹 & 멤버 레포지토리 생성

---
+++ Idol_Crawling/src/main/java/com/sokpulee/idolcrawling/crawling/repository/IdolGroupRepository.java
@@ -0,0 +1,7 @@
+package com.sokpulee.idolcrawling.crawling.repository;
+
+import com.sokpulee.idolcrawling.crawling.entity.IdolGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IdolGroupRepository extends JpaRepository<IdolGroup, Long> {
+}
