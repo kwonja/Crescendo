abd526884d38c2a0cb7d97ad56967b08c69f9176
kwonja <kownja87@gmail.com>
2024-08-07 13:43:00 +0900

    test : 빌드테스트

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Main() {
-  return <div className="text-blue-600 text-3xl font-bold underline">빌드가 성공적123</div>;
+  return <div className="text-blue-600 text-3xl font-bold underline">빌드가 성공적1234</div>;
 }
