807cc04007f528ad4b9b766482089487be8c7086
Sanizzang <tksl4732@gmail.com>
2024-08-09 13:58:57 +0900

    fix: 커뮤니티 아이돌그룹 조회 검색 기능 추가

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/idol/repository/IdolGroupRepository.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/idol/repository/IdolGroupRepository.java
@@ -1,6 +1,8 @@
 package com.sokpulee.crescendo.domain.idol.repository;
 
 import com.sokpulee.crescendo.domain.idol.entity.IdolGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +13,5 @@ public interface IdolGroupRepository extends JpaRepository<IdolGroup, Long> {
 
     List<IdolGroup> findAllByOrderByNameAsc();
 
+    Page<IdolGroup> findByNameContaining(String keyword, Pageable pageable);
 }
