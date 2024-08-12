759c8ad4f10590d5e3fd7dc1254bc6154624b5ff
김인제 <kij723@naver,com>
2024-08-12 09:40:29 +0900

    feat: 랜드마크 좌표 API 구현

--- Similarity_Analyze/app.py
+++ Similarity_Analyze/app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from service import analyze_service
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    video_url = "C:\\Users\\SSAFY\\Downloads\\구구단챌린지1.mp4"
-    return analyze_service.get_landmark_position(video_url)
+@app.route('/similarity/landmark', methods=['POST'])
+def get_landmarks():
+    data = request.json
+    video_url = data.get('video_url')
+
+    if not video_url:
+        return jsonify({"error": "video_url is required"}), 400
+
+    # 비디오에서 랜드마크 추출
+    landmarks = analyze_service.get_landmark_position(video_url)
+
+    # 랜드마크 데이터를 JSON 형식으로 반환
+    return jsonify({"landmarks": landmarks}), 200
 
 
 if __name__ == '__main__':
