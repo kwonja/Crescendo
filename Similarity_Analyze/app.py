0fe8308ad2bb3ceb31a3cc18e5aec221ebc03095
김인제 <kij723@naver,com>
2024-08-12 10:41:25 +0900

    refactor: 랜드마크 좌표 API 수정

--- Similarity_Analyze/app.py
+++ Similarity_Analyze/app.py
@@ -4,19 +4,40 @@ from service import analyze_service
 app = Flask(__name__)
 
 
-@app.route('/similarity/landmark', methods=['POST'])
+@app.route('/api/v1/challenge/similarity/landmark', methods=['POST'])
 def get_landmarks():
+    print("Request received")
+
     data = request.json
     video_url = data.get('video_url')
+
+    print(video_url)
 
     if not video_url:
         return jsonify({"error": "video_url is required"}), 400
 
-    # 비디오에서 랜드마크 추출
     landmarks = analyze_service.get_landmark_position(video_url)
 
-    # 랜드마크 데이터를 JSON 형식으로 반환
-    return jsonify({"landmarks": landmarks}), 200
+    response_data = {
+        "landmark_positions": [
+            {
+                "frame_number": frame_data['frame'],
+                "landmarks": [
+                    {
+                        "landmark_index": idx,
+                        "x": lm['x'],
+                        "y": lm['y'],
+                        "z": lm['z'],
+                        "visibility": lm['visibility']
+                    }
+                    for idx, lm in enumerate(frame_data['landmarks'])
+                ]
+            }
+            for frame_data in landmarks
+        ]
+    }
+
+    return jsonify(response_data), 200
 
 @app.route('/similarity/analyze', methods=['POST'])
 def analyze_similarity():
