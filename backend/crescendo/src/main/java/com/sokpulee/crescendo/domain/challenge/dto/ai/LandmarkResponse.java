66298706f15e4de3462a03ce84f20ba181302a00
Sanizzang <tksl4732@gmail.com>
2024-08-12 14:38:51 +0900

    fix: LandmarkResponse 수정

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/LandmarkResponse.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/LandmarkResponse.java
@@ -1,15 +1,18 @@
 package com.sokpulee.crescendo.domain.challenge.dto.ai;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class LandmarkResponse {
 
-    private List<LandmarkFrame> landmarkPositions;
+    @JsonProperty("landmark_positions")
+    private List<LandmarkFrame> landmarkPositions =  new ArrayList<>();
 
     public LandmarkResponse(List<LandmarkFrame> landmarkPositions) {
         this.landmarkPositions = landmarkPositions;
