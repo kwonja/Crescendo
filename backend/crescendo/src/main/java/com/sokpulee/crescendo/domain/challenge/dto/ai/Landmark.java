76e09b02221125ee6e9537c76dd8a7fa80dbdb7f
Sanizzang <tksl4732@gmail.com>
2024-08-12 14:21:50 +0900

    fix: Landmark 관련 dto 기본생성자 추가

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/Landmark.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/Landmark.java
@@ -1,8 +1,10 @@
 package com.sokpulee.crescendo.domain.challenge.dto.ai;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class Landmark {
 
     private int landmarkIndex;
--- backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/LandmarkFrame.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/domain/challenge/dto/ai/LandmarkFrame.java
@@ -1,10 +1,12 @@
 package com.sokpulee.crescendo.domain.challenge.dto.ai;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class LandmarkFrame {
 
     private int frameNumber;
