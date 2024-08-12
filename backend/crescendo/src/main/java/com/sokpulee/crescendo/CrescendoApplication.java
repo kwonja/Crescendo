58e9c994cb2c3e875ace7744691b530628b6bdb9
Sanizzang <tksl4732@gmail.com>
2024-08-12 12:26:40 +0900

    fix; RestTemplate Bean 생성 Configuration 추가

--- backend/crescendo/src/main/java/com/sokpulee/crescendo/CrescendoApplication.java
+++ backend/crescendo/src/main/java/com/sokpulee/crescendo/CrescendoApplication.java
@@ -2,10 +2,8 @@ package com.sokpulee.crescendo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @EnableScheduling
@@ -15,10 +13,4 @@ public class CrescendoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrescendoApplication.class, args);
 	}
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
-
 }
